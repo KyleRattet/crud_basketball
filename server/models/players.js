3800455e69715d9438464606670cb93f7fcfca96
KyleRattet <krattet@gmail.com>
2015-09-09 09:37:53 -0600

    deploy

--- server/models/players.js
+++ server/models/players.js
@@ -14,6 +14,6 @@ var Player = new Schema(
 // mongoose.connect(process.env.DB_HOST);
 
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://heroku_7qnkbcfj:mpqsvvh652adap0bmmqnv4okkn@ds047612.mongolab.com:47612/heroku_7qnkbcfj')
+mongoose.connect('mongodb://localhost/players'|| 'mongodb://heroku_7qnkbcfj:mpqsvvh652adap0bmmqnv4okkn@ds047612.mongolab.com:47612/heroku_7qnkbcfj')
 //players is the collections, Player is the schema
 module.exports = mongoose.model('players', Player);
