e3fb42322975c977f09322a1e8bf3e4c7fdb88ab
KyleRattet <krattet@gmail.com>
2015-09-09 15:29:56 -0600

    added mongolab

--- server/models/players.js
+++ server/models/players.js
@@ -14,6 +14,10 @@ var Player = new Schema(
 // mongoose.connect(process.env.DB_HOST);
 
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://heroku_7qnkbcfj:mpqsvvh652adap0bmmqnv4okkn@ds047612.mongolab.com:47612/heroku_7qnkbcfj')
+// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://heroku_7qnkbcfj:mpqsvvh652adap0bmmqnv4okkn@ds047612.mongolab.com:47612/heroku_7qnkbcfj')
+
+
+///for heroku
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/players")
 //players is the collections, Player is the schema
 module.exports = mongoose.model('players', Player);
